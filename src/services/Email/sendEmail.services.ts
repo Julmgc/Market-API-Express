95c09d8e80bed20d6908a16280b6525f8aef0c74
Julia Clepf <jumgc09@gmail.com>
2022-02-10 13:40:33 -0300

    fix: add try and catch to sendEmail service

--- src/services/Email/sendEmail.services.ts
+++ src/services/Email/sendEmail.services.ts
@@ -12,40 +12,44 @@ interface Request {
 
 export default class SendEmailService {
   public async execute({ email, text, name, subject }: Request): Promise<void> {
-    var mailer = nodemailer.createTransport({
-      host: "smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: "68ea8a3d948603",
-        pass: "7ee5e88f5a1daa",
-      },
-    });
-
-    mailer.use(
-      "compile",
-      hbs({
-        viewEngine: {
-          partialsDir: path.resolve(__dirname, "..", "..", "email"),
-          defaultLayout: undefined,
+    try {
+      var mailer = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "68ea8a3d948603",
+          pass: "7ee5e88f5a1daa",
         },
-        viewPath: path.resolve(__dirname, "..", "..", "email"),
-      })
-    );
-    const mail = {
-      from: "market_don't_answer@market.com",
-      to: email,
-      subject: subject,
-      template: "sendGenericemail",
-      context: {
-        name,
-        text,
-      },
-    };
+      });
 
-    mailer.sendMail(mail, (err, info) => {
-      if (err) {
-        throw new AppError("Error while sending  email", 500);
-      }
-    });
+      mailer.use(
+        "compile",
+        hbs({
+          viewEngine: {
+            partialsDir: path.resolve(__dirname, "..", "..", "email"),
+            defaultLayout: undefined,
+          },
+          viewPath: path.resolve(__dirname, "..", "..", "email"),
+        })
+      );
+      const mail = {
+        from: "market_don't_answer@market.com",
+        to: email,
+        subject: subject,
+        template: "sendGenericemail",
+        context: {
+          name,
+          text,
+        },
+      };
+
+      mailer.sendMail(mail, (err, info) => {
+        if (err) {
+          throw new AppError("Error while sending  email", 500);
+        }
+      });
+    } catch (error) {
+      throw new AppError((error as any).message, 401);
+    }
   }
 }
